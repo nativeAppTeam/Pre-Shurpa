c1844f83d2d7276c1d892584ca30e5ebee05c58e
Christopher Fleming <cfleming214@gmail.com>
2017-01-04 15:19:44 -0800

    adding nikol's work (create pages)

--- client/controllers/createController.js
+++ client/controllers/createController.js
@@ -3,6 +3,22 @@ angular
   .controller('createController', createController);
 
 function createController($scope, $location, ItinFactory) {
+  $scope.spots = ['spot1'];
+  $scope.addNewSpot= function(){
+    let newItemNo = $scope.spots.length+1;
+    $scope.spots.push('spot'+newItemNo);
+  }
 
+  $scope.removeChoice = function(){
+    let lastItem = $scope.spots.length-1;
+    $scope.spots.splice(lastItem);
+  }
 
+  $scope.setAuthorAndLocation= function(){
+    // itinAuthor
+    // authorLocation
+    // placeName
+    // location
+    // description
+  }
 }
