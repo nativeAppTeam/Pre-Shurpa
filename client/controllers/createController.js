faf76c0fc37eea6ebb43e5e7703385a96aeddc4b
nmarentes <nikolmarentes@gmail.com>
2017-01-06 09:44:48 -0800

    adding comments

--- client/controllers/createController.js
+++ client/controllers/createController.js
@@ -4,21 +4,26 @@ angular
 
 function createController($scope, $location, ItinFactory, $http) {
   $scope.spots = ['spot1'];
+  //addNewSpot lets you add additional stops on the Itinerary
   // $scope.addNewSpot= function(){
   //   let newItemNo = $scope.spots.length+1;
   //   $scope.spots.push('spot'+newItemNo);
   // }
-
+  
+  //removeChoice lets you remove those stops on the Itinerary
   // $scope.removeChoice = function(){
   //   let lastItem = $scope.spots.length-1;
   //   $scope.spots.splice(lastItem);
   // }
 
+  //updateScope is to set the scope from the ng-change inputs in the create.html file
   $scope.updateScope = function(inputName, inputValue){
     //console.log(inputName, inputValue)
     $scope[inputName] = inputValue;
    // console.log($scope[inputName])
   }
+
+  //save is necessary to run the post function in the ItinFactory 
   $scope.save = function () {
     //console.log($scope.title, $scope.author, $scope.authorLocation, $scope.authorZip, $scope.stop1placeName, $scope.stop1description);
     ItinFactory.post(
@@ -42,6 +47,8 @@ function createController($scope, $location, ItinFactory, $http) {
       alert('Itinerary Created!!!')
     })
   }
+
+  //search location is used mostly to create the authorZip, which is used to filter through the database for the zipcode
   $scope.searchLocation = function (x) {
     $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + x + '&key=AIzaSyDRjb5435OyNsX2BO4QM7vR-84vvUuzTBM')
       .success(function (data) {
@@ -50,6 +57,8 @@ function createController($scope, $location, ItinFactory, $http) {
         console.log($scope.authorZip)
       });
   };
+
+  //getLocation allows us to get the current position of the user as well as create the authorZip from the geolocation
   $scope.getLocation = function () {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
@@ -58,7 +67,6 @@ function createController($scope, $location, ItinFactory, $http) {
           $scope.authorZip= data.results[0].formatted_address.slice(-10).slice(0,5);
           console.log($scope.authorZip)
         });
-
         // console.log(addressObj)        
         $scope.location = position.coords.latitude + ", " + position.coords.longitude;
         //  console.log($scope.location)
