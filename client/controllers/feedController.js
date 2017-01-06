a863124f22c9ee78613aa318a9d28ddd7aca0f44
Christopher Fleming <cfleming214@gmail.com>
2017-01-06 10:20:21 -0800

    comments for iteration

--- client/controllers/feedController.js
+++ client/controllers/feedController.js
@@ -4,20 +4,13 @@ angular
 
 function feedController($scope, ItinFactory, $animate) {
 
+//setting scope to ItinFactorie's currentItins. Giving us access to that array of itineraries
 $scope.currentItins = ItinFactory.currentItins;
 $scope.searchZip = ItinFactory.searchZip;
-console.log($scope.searchZip);
-// $scope.nums = [1,2,3];
-// $scope.title = 'Marina Del Rey';
-// $scope.zip = 90508;
-// $scope.author = "Chris Fleming";
-// $scope.name = "Whole Foods";
-// $scope.location = "Jefferson BLVD";
-// $scope.description = "Whole foods is a wonderful market here in town.";
 
-  // for(let i = 0; i < ItinFactory.currentItins.data; i += 1) {
-  //   itinArr.push(ItinFactory.currentItins.data[i]);
-  // }
+//logging searchZip
+console.log($scope.searchZip);
+
 
 
 }
