2c470979e84d5ae48b6f304be29ae7faf17accf6
nmarentes <nikolmarentes@gmail.com>
2017-01-05 17:58:29 -0800

    create page update

--- client/controllers/createController.js
+++ client/controllers/createController.js
@@ -1,24 +1,67 @@
 angular
-  .module('solo.createController', ['ngRoute', 'solo.ItinFactory'])
+  .module('solo.createController', ['ngRoute', 'ngMap', 'solo.ItinFactory'])
   .controller('createController', createController);
 
-function createController($scope, $location, ItinFactory) {
+function createController($scope, $location, ItinFactory, $http) {
   $scope.spots = ['spot1'];
-  $scope.addNewSpot= function(){
-    let newItemNo = $scope.spots.length+1;
-    $scope.spots.push('spot'+newItemNo);
-  }
+  // $scope.addNewSpot= function(){
+  //   let newItemNo = $scope.spots.length+1;
+  //   $scope.spots.push('spot'+newItemNo);
+  // }
 
-  $scope.removeChoice = function(){
-    let lastItem = $scope.spots.length-1;
-    $scope.spots.splice(lastItem);
-  }
+  // $scope.removeChoice = function(){
+  //   let lastItem = $scope.spots.length-1;
+  //   $scope.spots.splice(lastItem);
+  // }
 
-  $scope.setAuthorAndLocation= function(){
-   // ItinFactory.post($scope.itinAuthor, $scope.authorLocation)
+  $scope.updateScope = function(inputName, inputValue){
+    //console.log(inputName, inputValue)
+    $scope[inputName] = inputValue;
+   // console.log($scope[inputName])
   }
-$scope.save= function(){
-  //  ItinFactory.post($scope.placeName, $scope.location, $scope.description)
-}
+  $scope.save = function () {
+    console.log($scope.title, $scope.author, $scope.authorLocation, $scope.authorZip, $scope.stop1placeName, $scope.stop1description);
+    ItinFactory.post(
+      $scope.title,
+      $scope.author,
+      $scope.authorLocation,
+      $scope.authorZip,
+      $scope.stop1placeName,
+      $scope.stop1location,
+      $scope.stop1description,
+      $scope.stop2placeName,
+      $scope.stop2location,
+      $scope.stop2description,
+      $scope.stop3placeName,
+      $scope.stop3location,
+      $scope.stop3description,
+      $scope.stop4placeName,
+      $scope.stop4location,
+      $scope.stop4description
+    )
+  }
+  $scope.searchLocation = function (x) {
+    $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + x + '&key=AIzaSyDRjb5435OyNsX2BO4QM7vR-84vvUuzTBM')
+      .success(function (data) {
+        console.log(data);
+        $scope.authorZip = data.results[0].formatted_address.slice(-10).slice(0,5);
+        console.log($scope.authorZip)
+      });
+  };
+  $scope.getLocation = function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyDRjb5435OyNsX2BO4QM7vR-84vvUuzTBM').success(function (data) {
+          $scope.authorLocation= data.results[0].formatted_address;
+          $scope.authorZip= data.results[0].formatted_address.slice(-10).slice(0,5);
+          console.log($scope.authorZip)
+        });
 
+        // console.log(addressObj)        
+        $scope.location = position.coords.latitude + ", " + position.coords.longitude;
+        //  console.log($scope.location)
+      });
+    }
+    console.log("Running get Location")
+  };
 }
--- client/factories/itinFactory.js
+++ client/factories/itinFactory.js
@@ -1,17 +1,34 @@
+
 angular
-  .module('solo.ItinFactory', [])
+  .module('solo.ItinFactory', ['solo.createController'])
   .factory('ItinFactory', ItinFactory);
 
-function ItinFactory() {
+function ItinFactory($http) {
   return {
-    post: function () {
-      itinerary.create({
-        id: req.spot,
-        author: req.author,
-        location: req.location,
-        description: req.description
+    post: function (title, author, authorLocation, authorZip, stop1placeName, stop1location, stop1description,stop2placeName, stop2location, stop2description, stop3placeName, stop3location, stop3description, stop4placeName, stop4location, stop4description) {
+      return $http({
+        method: 'POST',
+        url: 'http://localhost:3000/create',
+        data: {
+        title: title,
+        author: author,
+        authorLocation:  authorLocation,
+        authorZip: authorZip,
+        stop1placeName: stop1placeName,
+        stop1location: stop1location,
+        stop1description: stop1description,
+        stop2placeName: stop2placeName,
+        stop2location: stop2location,
+        stop2description: stop2description,
+        stop3placeName:  stop3placeName,
+        stop3location: stop3location,
+        stop3description: stop3description,
+        stop4placeName: stop4placeName,
+        stop4location: stop4location,
+        stop4description: stop4description
+      }
       })
     }
   }
-
 }
+
